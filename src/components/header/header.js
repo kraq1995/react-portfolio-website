5786cf3b1a1cd9374a0d4e4d115353c13ac1a27f
krakus <m.krakowiak@hotmail.com>
2021-03-09 16:19:53 +0100

    initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <header className="App-header">
+<div class="typewriter"><h1>Looking for software engineers?</h1></div>
+<div class="typewriter"><h2>biuro@kraqsoft.pl / +48 737-487-922</h2></div>
+</header>
+      </React.Fragment>
+    );
+  }
+}
